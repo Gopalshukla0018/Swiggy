4b34c90566f4bc3ef2599185f25eebbcf7c941fa
gopalshukla0018 <gopalshukla0018@gmail.com>
2025-06-30 03:32:26 +0530

    added menu option

--- src/Components/RestaurantMenu.jsx
+++ src/Components/RestaurantMenu.jsx
@@ -9,23 +9,38 @@ const RestaurantMenu = () => {
   }, []);
   const fetchMenu = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.4552521&lng=77.5046101&restaurantId=625180&catalog_qa=undefined&submitAction=ENTER"
+      // "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.4552521&lng=77.5046101&restaurantId=625180&catalog_qa=undefined&submitAction=ENTER"
+      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.4552521&lng=77.5046101&restaurantId=959142&catalog_qa=undefined&submitAction=ENTER"
     );
     const json = await data.json();
     console.log(json);
     setInfo(json.data);
   };
 
-  return resInfo === null ? (
-    <Shimmer />
-  ) : (
+  if (resInfo === null) {
+    return <Shimmer />;
+  }
+
+  const { name, cuisines, costForTwoMessage } =
+    resInfo?.cards?.[2]?.card?.card?.info || {};
+  const itemCards =
+    resInfo?.cards?.[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[3]?.card
+      ?.card.categories?.[0]?.itemCards || [];
+
+  return (
     <div className="restaurant-menu">
-      <h1>{resInfo?.cards?.[2]?.card?.card?.info?.name}</h1>
+      <h1>{name}</h1>
+      <p>
+        {cuisines?.join(",")} - {costForTwoMessage}
+      </p>
       <h2>Menu</h2>
       <ul>
-        <li>Biryani</li>
-        <li>Butter Naan</li>
-        <li>Paneer Tikka</li>
+        {itemCards.map((item) => (
+          <li key={item?.card?.info?.id}>
+            {item?.card?.info?.name} - Rs.{" "}
+            {item?.card?.info?.defaultPrice / 100}
+          </li>
+        ))}
       </ul>
     </div>
   );
