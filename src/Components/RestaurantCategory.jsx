43ea8b914c0a6d659118398698559ddba3d4abd8
gopalshukla0018 <gopalshukla0018@gmail.com>
2025-07-05 18:21:25 +0530

    accordion updated

--- src/Components/RestaurantCategory.jsx
+++ src/Components/RestaurantCategory.jsx
@@ -1,13 +1,19 @@
 const RestaurantCategory = ({ data }) => {
   console.log(data);
 
+  const handleOnclick=()=>{
+    console.log("down arrow clicked");
+    
+    
+  }
+
   return (
     <div>
       <div className="flex items-center justify-between w-6/12 px-4 py-3 mx-auto my-4 transition duration-200 rounded-lg shadow-sm cursor-pointer bg-gray-50 hover:bg-gray-100">
         <span className="text-lg font-semibold text-gray-800">
           {data.title} ({data.itemCards.length})
         </span>
-        <span className="text-xl">🔽</span>
+        <span className="text-xl" onClick={handleOnclick}>🔽</span>
       </div>
     </div>
   );
