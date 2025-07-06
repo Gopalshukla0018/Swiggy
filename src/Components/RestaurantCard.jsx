43656c88a15d49700898ed558fa1231723aef692
gopalshukla0018 <gopalshukla0018@gmail.com>
2025-07-07 03:33:56 +0530

    add css

--- src/Components/RestaurantCard.jsx
+++ src/Components/RestaurantCard.jsx
@@ -14,15 +14,22 @@ const RestaurentCard = (props) => {
     deliveryTime,
   } = resData.info;
   return (
-    <div className="w-full max-w-xs sm:max-w-sm md:max-w-xs bg-white p-4 m-4 rounded-lg shadow hover:scale-105 transition-transform duration-200">
+    <div className="flex flex-col items-stretch w-full max-w-xs p-4 m-4 transition-transform duration-200 bg-white rounded-lg shadow sm:max-w-sm md:max-w-xs hover:scale-105">
       <img
-        className="res-logo w-full h-40 object-cover rounded"
+        className="object-cover w-full h-40 rounded res-logo"
         src={CDN_URL + cloudinaryImageId}
         alt="res-logo"
       />
-      <h3 className="font-bold text-lg mt-2">{name}</h3>
-      <h4 className="text-gray-700 text-sm">{cuisines.join(", ")}</h4>
-      <h4 className="text-yellow-600 font-semibold">{avgRating} stars</h4>
+      <h3 className="mt-2 text-lg font-bold truncate" title={name}>
+        {name}
+      </h3>
+      <h4
+        className="text-sm text-gray-700 truncate"
+        title={cuisines.join(", ")}
+      >
+        {cuisines.join(", ")}
+      </h4>
+      <h4 className="font-semibold text-yellow-600">{avgRating} stars</h4>
       <h4 className="text-gray-800">{costForTwo}</h4>
       <h4 className="text-gray-500">{deliveryTime}</h4>
     </div>
