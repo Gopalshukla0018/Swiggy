1e1ee6a6b74c7e5feb8476501d153f201e9f65a1
gopalshukla0018 <gopalshukla0018@gmail.com>
2025-07-15 20:50:48 +0530

    image fix in right

--- src/Components/ItemList.jsx
+++ src/Components/ItemList.jsx
@@ -2,33 +2,35 @@ import CDN_URL from "../utils/constants";
 const ItemList = ({ items }) => {
   console.log(items);
   return (
-    <div className="flex flex-col ">
+    <div >
       {items.map((item) => (
         <div
           key={item.card.info.id}
-          className="p-2 m-2 border-gray-200  border-b-2 text-left"
+          className="p-2 m-2 border-gray-200  border-b-2 text-left flex justify-between"
         >
-         
-          <div className="py-2">
-            <span> {item.card.info.name} </span>
-            <span
-              className="text-lg font-semibold "
-              style={{ color: "rgba(2, 6, 12, 0.75)" }}
-            >
-              {item.card.info.name}
-            </span>
-            <span>
-              - &#8377;
-              {item.card.info.price / 100 || item.card.info.defaultPrice / 100}
-            </span>
-             <div>
+          <div>
+            <div className="py-2">
+              <span> {item.card.info.name} </span>
+              <span   
+                className="text-lg font-semibold "
+                style={{ color: "rgba(2, 6, 12, 0.75)" }}
+              >
+                {item.card.info.name}
+              </span>
+              <span>
+                - &#8377;
+                {item.card.info.price / 100 ||
+                  item.card.info.defaultPrice / 100}
+              </span>
+            </div>
+            <p className="text-xs">{item.card.info.description}</p>
+          </div>
+          <div>
             <img
               src={CDN_URL + item.card.info.imageId}
               className="w-30 rounded-xl block ml-auto "
             />
           </div>
-          </div>
-          <p className="text-xs">{item.card.info.description}</p>
         </div>
       ))}
     </div>
