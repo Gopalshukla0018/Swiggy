6c9a485908e1cc0c938387cb1902b4018cf0e6a1
gopalshukla0018 <gopalshukla0018@gmail.com>
2025-07-25 23:38:56 +0530

    commit

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -2,8 +2,8 @@ const contact=()=>{
     return(
         
         <div>
-           {/* <h1>  contact us on gopalshukla0018@gmail.com</h1> */}
-           
+           <h1>  contact us on gopalshukla0018@gmail.com</h1>
+
            
             
         </div>
