85f387467f968f44b29dcb62da93d518dbf0c0e4
gopalshukla0018 <gopalshukla0018@gmail.com>
2025-08-01 12:25:23 +0530

    adding cart using redux

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -4,8 +4,8 @@ const cart = () => {
   const cartItems = useSelector((store) => store.cart.items);
 
   return (
-    <div className="p-4 m-4 text-center ">
-      <h1 className="text-xl font-bold">Cart</h1>
+    <div className="flex w-6/12 p-4 m-auto text-center ">
+     
       {<ItemList items={cartItems} />}
     </div>
   );
