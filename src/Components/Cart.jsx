56a0ab3171ea7d6f76dc8271de9c557bf27b5da7
gopalshukla0018 <gopalshukla0018@gmail.com>
2025-08-01 13:17:35 +0530

    cart is on diead h1 add when cart is empty

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,12 +1,12 @@
 import { useDispatch, useSelector } from "react-redux";
 import ItemList from "./ItemList";
 import { clearCart } from "../utils/cartSlice";
+import { HiH1 } from "react-icons/hi2";
 const cart = () => {
   const cartItems = useSelector((store) => store.cart.items);
   const dispatch = useDispatch();
   const handleClearCart = () => {
     dispatch(clearCart());
-    
   };
   return (
     <>
@@ -14,11 +14,14 @@ const cart = () => {
         {<ItemList items={cartItems} />}
       </div>
       <button
-         className="absolute p-2 text-white transition duration-150 ease-in-out transform bg-gray-600 border rounded-md cursor-pointer hover:bg-gray-900 right-10 bottom-3 active:scale-95 active:bg-gray-700"
+        className="absolute p-2 text-white transition duration-150 ease-in-out transform bg-gray-600 border rounded-md cursor-pointer hover:bg-gray-900 right-10 bottom-3 active:scale-95 active:bg-gray-700"
         onClick={handleClearCart}
       >
         {" "}
         Clear Cart
+        {cartItems.length === 0 && (
+          <h1 >🛒 Cart is on a diet! Add some delicious items 😎</h1>
+        )}
       </button>
     </>
   );
