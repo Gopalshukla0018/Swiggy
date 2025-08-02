5ebcc7d5ce811d752880b65557262085b1edddba
gopalshukla0018 <gopalshukla0018@gmail.com>
2025-08-02 18:20:29 +0530

    make cart better

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -12,6 +12,7 @@ const cart = () => {
       <div className="relative w-6/12 p-4 m-auto text-center ">
         {<ItemList items={cartItems} />}
       </div>
+      
       <button
         className="absolute p-2 text-white transition duration-150 ease-in-out transform bg-gray-600 border rounded-md cursor-pointer hover:bg-gray-900 right-10 bottom-3 active:scale-95 active:bg-gray-700"
         onClick={handleClearCart}
