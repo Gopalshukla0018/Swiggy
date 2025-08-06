f082609b452a97f5407c3e84cf86a800c32b00bc
gopalshukla0018 <gopalshukla0018@gmail.com>
2025-08-06 21:02:42 +0530

    improve search suggestions and add logic for press Enter Key on input

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -8,8 +8,6 @@ const search = ({ Listofrestaurant, setfilteredRestaurant }) => {
     return res.info.name.toLowerCase().includes(searchText.toLowerCase());
   });
 
-
-
   const handleSelect = (resname) => {
     setsearchText(resname);
     const filteredList = Listofrestaurant.filter((res) => {
@@ -18,47 +16,44 @@ const search = ({ Listofrestaurant, setfilteredRestaurant }) => {
     setfilteredRestaurant(filteredList);
     // Clear suggestions after selection
     setsearchText("");
-    // Optionally, you can also clear the suggestions if needed
-   
-   
-  }
+  };
+  const performSearch = () => {
+    const filteredList = Listofrestaurant.filter((res) => {
+      return res.info.name.toLowerCase().includes(searchText.toLowerCase());
+    });
+    setfilteredRestaurant(filteredList);
+  };
 
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      performSearch();
+    }
+    // if the Enter key is pressed call performSearch function
+  };
 
   return (
     <>
       <div className="flex items-center gap-2">
         <div className="relative ">
-            
-           <input
-          type="text"
-          className="w-40 sm:w-64 p-2 border border-solid border-black rounded-lg"
-          placeholder="Search for restaurants"
-          value={searchText}
-          onChange={(e) => {
-            const text = e.target.value;
-            setsearchText(e.target.value);
-          }}
-        />
-        {searchText.length > 0 && (
-          <Suggestions
-            suggestions={suggestions}
-            onSelect={handleSelect}
+          <input
+            type="text"
+            className="w-40 sm:w-64 p-2 border border-solid border-black rounded-lg"
+            placeholder="Search for restaurants"
+            value={searchText}
+            onChange={(e) => {
+              const text = e.target.value;
+              setsearchText(e.target.value);
+            }}
+            onKeyDown={handleKeyDown}
           />
-        )}
-
+          {searchText.length > 0 && (
+            <Suggestions suggestions={suggestions} onSelect={handleSelect} />
+          )}
         </div>
-       
 
         <button
-           className="px-4 py-2 bg-green-100 rounded-lg hover:bg-green-200"
-          onClick={() => {
-            const filteredList = Listofrestaurant.filter((res) => {
-              return res.info.name
-                .toLowerCase()
-                .includes(searchText.toLowerCase());
-            });
-            setfilteredRestaurant(filteredList);
-          }}
+          className="px-4 py-2 bg-green-100 rounded-lg hover:bg-green-200"
+          onClick={performSearch}
         >
           Search
         </button>
